39faa6e52f8c6f0fbc7dcace707c668906e70d30
danielfdch <daniel.flor3s.d@gmail.com>
2022-09-26 22:23:16 -0700

    Initial README file

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
 
+function generatePassword(){
+  return "Password";
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
